bb65ad1f2d2c7563fb04da4d3af13f289604ba90
Mateusz <37794409+LemanskiM@users.noreply.github.com>
2022-06-12 18:18:40 +0200

    Add files via upload

---
+++ dodawanie_danych_do_plikow_powczesniejszymwyczyszczeniu.py
@@ -0,0 +1,15 @@
+filename = "c:\\python\\artur.txt"
+
+line = "Europe"
+
+cities = ["12","212","saw"]
+
+file = open(filename, "w")
+
+file.write(line)
+file.write("\n")
+#file.writelines(cities)
+for city in cities:
+    file.write("\n"+city)
+
+file.close()
